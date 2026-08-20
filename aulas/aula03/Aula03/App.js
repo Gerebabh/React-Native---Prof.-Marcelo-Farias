b85cea6171d068466d47cbd625161ef6d96280a6
gerebabh <geralucio@gmail.com>
2026-08-20 14:05:34 -0300

    Atualização aula04

--- aulas/aula03/Aula03/App.js
+++ aulas/aula03/Aula03/App.js
@@ -6,11 +6,24 @@ import { rotulo_input_meta, rotulo_lista_metas, rotulo_btn_cadastro_meta } from 
 
 export default function App() {
   return (
-gi
       <View style={styles.mainContainer}>
-        <TextInput placeholder={rotulo_input_meta} />
-        <Button title={rotulo_btn_cadastro_meta} />
-        <Text>{rotulo_lista_metas} </Text>
+        <View style={{
+          flex: 1,
+          flexDirection: 'row',
+          justifyContent: 'space-between'}}>
+          <View style={{width: '65%'}}>
+            <TextInput 
+              style={styles.inputText}
+              placeholder={rotulo_input_meta} 
+            />
+          </View>
+          <View style={{width: "30%"}}>
+            <Button title={rotulo_btn_cadastro_meta} />
+          </View>
+        </View>
+        <View style={styles.metaContainer}>
+          <Text>{rotulo_lista_metas} </Text>
+        </View>
       </View>
 
   );
@@ -18,13 +31,20 @@ gi
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: colors.variables.black,
     alignItems: 'center',
     justifyContent: 'center',
   },
   mainContainer: {
-    padding: 30
+    padding: 30,
+    flex: 1,
+    flexDirection: 'column'
+  },
+  inputText: {
+    borderColor: colors.gray[300],
+    borderWidth: 1,
+    borderRadius: 6,
+  },
+  metaContainer: {
+    flex: 1
   }
-
 });
